d303f1e616e04ec9ac8e8eeb87c4c743540b1bca
saewoohan <hso2341@hanyang.ac.kr>
2024-01-27 00:44:59 +0900

    feat: added plogging history

--- trab_front/lib/feature/setting/presentation/view/plogging_record_screen.dart
+++ trab_front/lib/feature/setting/presentation/view/plogging_record_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:trab_front/feature/common/widget/custom_appbar.dart';
+import 'package:trab_front/feature/plogging/data/model/plogging_model.dart';
 import 'package:trab_front/feature/plogging/domain/plogging_domain.dart';
 import 'package:trab_front/feature/setting/presentation/widget/plogging_record_container.dart';
 import 'package:trab_front/helpers/constants/app_colors.dart';
@@ -19,16 +20,16 @@ class PloggingRecordScreen extends ConsumerStatefulWidget {
 class _PloggingRecordScreenState extends ConsumerState<PloggingRecordScreen> {
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      print("여기");
       ref.read(ploggingControllerProvider.notifier).getPloggingList();
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    List<PloggingModel> ploggingList =
+        ref.watch(ploggingControllerProvider).ploggingList;
     return Scaffold(
       appBar: CustomAppBar(
         title: AppStrings.ploggingRecord,
@@ -41,9 +42,11 @@ class _PloggingRecordScreenState extends ConsumerState<PloggingRecordScreen> {
         ),
       ),
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [ploggingRecordContainer()],
+        child: ListView.builder(
+          itemCount: ploggingList.length,
+          itemBuilder: (context, index) {
+            return ploggingRecordContainer(plogging: ploggingList[index]);
+          },
         ),
       ),
     );
--- trab_front/lib/feature/setting/presentation/widget/plogging_record_container.dart
+++ trab_front/lib/feature/setting/presentation/widget/plogging_record_container.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:trab_front/feature/plogging/data/model/plogging_model.dart';
 import 'package:trab_front/helpers/constants/app_colors.dart';
 import 'package:trab_front/helpers/constants/app_images.dart';
 import 'package:trab_front/helpers/constants/app_typography.dart';
+import 'package:trab_front/helpers/constants/strings.dart';
+import 'package:trab_front/helpers/extensions/string_extension.dart';
 
-Widget ploggingRecordContainer() {
+Widget ploggingRecordContainer({required PloggingModel plogging}) {
   return Padding(
     padding: EdgeInsets.symmetric(horizontal: 15.w),
     child: Container(
@@ -15,59 +18,91 @@ Widget ploggingRecordContainer() {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 16.h,
-              ),
-              SizedBox(
-                height: 22.h,
-                child: Text(
-                  "DECEMBER 23, 2023",
-                  style: AppTypography.mainSubtitle
-                      .copyWith(color: AppColors.grey2),
+          SizedBox(
+            width: 200.w,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 16.h,
                 ),
-              ),
-              SizedBox(
-                height: 2.h,
-              ),
-              SizedBox(
-                height: 22.h,
-                child: Text(
-                  "마장동 트래비 간식사냥",
-                  style: AppTypography.subTitle_1
-                      .copyWith(color: AppColors.textColor_1),
+                SizedBox(
+                  height: 22.h,
+                  child: Text(
+                    plogging.runDate.getDateUpperString,
+                    style: AppTypography.mainSubtitle
+                        .copyWith(color: AppColors.grey2),
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 7.h,
-              ),
-              Row(
-                children: [
-                  SizedBox(
-                    height: 22.h,
-                    child: Text(
-                      "0.00 km",
-                      style:
-                          AppTypography.body.copyWith(color: AppColors.grey2),
+                SizedBox(
+                  height: 2.h,
+                ),
+                SizedBox(
+                  width: 180.w,
+                  height: 22.h,
+                  child: Text(
+                    plogging.runName,
+                    overflow: TextOverflow.ellipsis,
+                    style: AppTypography.subTitle_1
+                        .copyWith(color: AppColors.textColor_1),
+                  ),
+                ),
+                SizedBox(
+                  height: 7.h,
+                ),
+                Row(
+                  children: [
+                    ConstrainedBox(
+                      constraints: BoxConstraints(
+                        maxHeight: 22.h,
+                        minHeight: 22.h,
+                        maxWidth: 65.w,
+                      ),
+                      child: Text(
+                        "${plogging.runRange} ",
+                        overflow: TextOverflow.ellipsis,
+                        style:
+                            AppTypography.body.copyWith(color: AppColors.grey2),
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    width: 54.w,
-                  ),
-                  SizedBox(
-                    height: 22.h,
-                    child: Text(
-                      "0 간식",
-                      style:
-                          AppTypography.body.copyWith(color: AppColors.grey2),
+                    SizedBox(
+                      width: 1.w,
                     ),
-                  ),
-                ],
-              )
-            ],
+                    SizedBox(
+                      height: 22.h,
+                      child: Text(
+                        AppStrings.kiloMeter,
+                        style:
+                            AppTypography.body.copyWith(color: AppColors.grey2),
+                      ),
+                    ),
+                    const Spacer(),
+                    ConstrainedBox(
+                      constraints: BoxConstraints(
+                        maxHeight: 22.h,
+                        minHeight: 22.h,
+                        maxWidth: 65.w,
+                      ),
+                      child: Text(
+                        "${plogging.trabSnack} ",
+                        overflow: TextOverflow.ellipsis,
+                        style:
+                            AppTypography.body.copyWith(color: AppColors.grey2),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 22.h,
+                      child: Text(
+                        AppStrings.snack,
+                        style:
+                            AppTypography.body.copyWith(color: AppColors.grey2),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
           SizedBox(
             width: 57.w,
